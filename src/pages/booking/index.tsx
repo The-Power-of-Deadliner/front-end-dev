f4b360fee34f433c60aa9e63d1185aa64d44c143
Bunga Amalia <bungasokia@gmail.com>
2019-11-15 06:53:50 +0700

    Add modal

--- src/pages/booking/index.tsx
+++ src/pages/booking/index.tsx
@@ -33,10 +33,91 @@ const Loading = styled.div`
   margin: 575px 155px;
 `;
 
+const CardContainer = styled.div`
+  position: absolute;
+  width: 333px;
+  height: 241px;
+  left: 14px;
+  top: 200px;
+  padding: 20px;
+  background: #ffffff;
+  border: 1px solid #e0e0e0;
+  box-sizing: border-box;
+  box-shadow: 0px 5px 4px rgba(0, 0, 0, 0.05), 0px 0px 4px rgba(0, 0, 0, 0.1);
+  border-radius: 5px;
+  text-align: center;
+  padding: 40px 0;
+`;
+
+const ContainerRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-left: auto;
+  margin-right: auto;
+`;
+const PrimaryButton = styled.button`
+  background: #00b14d;
+  color: white;
+  width: 143.6px;
+  height: 43px;
+  font-size: 16px;
+  padding: 0.25em 1em;
+  border-radius: 5px;
+  margin-top: 20px;
+  margin-left: 4px;
+`;
+const SecondaryButton = styled.button`
+  background: white;
+  border: 1px solid #bdbdbd;
+  box-sizing: border-box;
+  border-radius: 5px;
+  width: 143.6px;
+  height: 43px;
+  font-size: 16px;
+  padding: 0.25em 1em;
+  border-radius: 5px;
+  margin-top: 20px;
+  margin-right: 4px;
+  margin-left: 18px;
+`;
+
+const Overlay = styled.div`
+  position: absolute;
+  width: 362px;
+  height: 640px;
+  left: -2px;
+  top: 0px;
+  /* Overlay */
+  background: linear-gradient(
+    180deg,
+    rgba(55, 59, 70, 0.4) 0%,
+    rgba(55, 59, 70, 0.4) 100%
+  );
+`;
+
+const ModalContainer = styled.div`
+  position: absolute;
+  font-family: 'Open Sans', sans-serif;
+  z-index: 3;
+`;
+
 export default function Page() {
   return (
     <>
       <ScreenContainer>
+        <ModalContainer>
+          <Overlay></Overlay>
+          <CardContainer>
+            <h2>Business purpose booking?</h2>
+            <h5>We find out that your booking is in </h5>
+            <h5>business hours (8AM-6PM). We can</h5>
+            <h5>help you switch to business tag.</h5>
+            <ContainerRow>
+              <SecondaryButton>Ignore</SecondaryButton>
+              <PrimaryButton>Switch Tag</PrimaryButton>
+            </ContainerRow>
+          </CardContainer>
+        </ModalContainer>
         <Payment>
           <Link to='/payment'>
             <ShapeButton />
