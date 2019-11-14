7ef6323b6dd7949bdd9b767b462c287ad4604ac9
aabccd021 <aabccd021@gmail.com>
2019-11-15 05:31:29 +0700

    aabccd

--- src/pages/recommend/index.tsx
+++ src/pages/recommend/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+// import React from 'react';
+import React, { Component } from 'react'
 import styled from 'styled-components';
 import Button from 'components/button';
 import { Link } from 'react-router-dom';
 import recommendBg from 'images/recommend.png';
+import axios from 'axios';
 
 const Test = styled.div`
   text-align: center;
@@ -15,17 +17,61 @@ const Screen = styled.img`
   margin: auto;
 `;
 
-export default function Page() {
-  return (
-    <>
-      <Screen src={recommendBg} alt='recommendBg'></Screen>
-      <Test>
-        Recommend
-        <br />
-        <Link to='/'>
-          <Button buttonText='Go to home' />
-        </Link>
-      </Test>
-    </>
-  );
+interface ServerResponse {
+  data: ServerData
 }
+
+interface ServerData {
+  source: string
+  dest: string
+  cost: number
+  schedule: string
+  vehicle: string
+  distance: number
+  duration: number
+}
+
+type PageState = {
+  data: any[]
+}
+
+class Page extends Component<{}, PageState> {
+
+  componentWillMount() {
+    this.setState({
+      data:[]
+    })
+ 
+    axios.get(
+      'https://hikeathon-functions.azurewebsites.net/api/leaveReccomendation',
+    ).then((response) => {
+      this.setState({
+        data : response.data.leavingTimes
+      })
+      console.log(this.state.data)
+    });
+  };
+
+  render() {
+
+    return (
+      <>
+        <Screen src={recommendBg} alt='recommendBg'></Screen>
+        {this.state.data.length === 0? "": this.state.data[0].source}
+        {this.state.data.length === 0? "": this.state.data[1].source}
+        {this.state.data.length === 0? "": this.state.data[2].source}
+        {this.state.data.length === 0? "": this.state.data[3].source}
+        <Test>
+          Recommend
+          <br />
+          <Link to='/'>
+            <Button buttonText='Go to home' />
+          </Link>
+        </Test>
+      </>
+    );
+  }
+
+}
+
+export default Page;
